ef3ce1ae7de98b384e153835ac384437b192fe70
Alrik <alrik11es@gmail.com>
2012-01-19 10:15:28 +0100

    Creando proyecto de visual basic.

---
+++ Aurora/Aurora/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Aurora")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("ColdStar Studios")> 
+<Assembly: AssemblyProduct("Aurora")> 
+<Assembly: AssemblyCopyright("Copyright © ColdStar Studios 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("a3dc6b08-22cd-40a7-b650-c2fbea0bc47b")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
